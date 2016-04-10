754263979c637566164c7181d6c7fb3193f131e5
Shugabuga <jeffreydivi@icloud.com>
2016-04-09 20:12:45 -0400

    Turned off draggable UI, too laggy.

---
+++ Chrome/Ycode.js
@@ -0,0 +1,84 @@
+// My code
+
+var BetterGFMotesSelectScreen = document.createElement("IFRAME");
+BetterGFMotesSelectScreen.setAttribute("src", "https://j-co.ga/BetterGFMotes/Preview/index.html");
+BetterGFMotesSelectScreen.setAttribute("width", "400px");
+BetterGFMotesSelectScreen.setAttribute("height", "500px");
+BetterGFMotesSelectScreen.setAttribute("id", "BetterGFMotesSelectScreen")
+BetterGFMotesSelectScreen.setAttribute("class", "BetterGFMotesSelectScreen")
+document.body.appendChild(BetterGFMotesSelectScreen);
+
+/*
+var BetterGFMotesBTN = document.createElement("BUTTON"); //Create button
+
+BetterGFMotesBTN.setAttribute("id", "BetterGFMotesBTN");
+BetterGFMotesBTN.setAttribute("class", "BetterGFMotesBTN");// Set button ID
+BetterGFMotesBTN.setAttribute("style", "position:absolute;z-index:102128917297"); // Set CSS
+BetterGFMotesBTN.setAttribute("onClick", ""); // Set code to run on button click
+
+var BetterGFMotesBTNt = document.createTextNode("jda3GFMotes"); // Set button label
+
+BetterGFMotesBTN.appendChild(BetterGFMotesBTNt); // Actually adds button label from above ^
+document.body.appendChild(BetterGFMotesBTN); //Add button to body
+
+*/
+
+$( "#BetterGFMotesSelectScreen" ).draggable();
+$( "#BetterGFMotesSelectScreen" ).resizable();
+
+//$( "#BetterGFMotesBTN" ).draggable();
+
+//var divs = document.getElementsByTagName("BetterGFMotesBTN");
+//BetterGFMotesBTN.parentNode.insertBefore(BetterGFMotesBTN, header); // Choose location where button will go
+
+
+
+//some BetterPonyMotes code that injects code into Reddit?
+
+//document.getElementById("header".appendChild(BetterGFMotesBTN));
+
+//document.getElementsByClassName("usertext-buttons") = "Hello World!";
+
+
+
+/*
+var cssId = 'myCss'; 
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://j-co.ga/BetterGFMotesDB/css/fonts.css';
+    link.media = 'all';
+    head.appendChild(link);
+} */
+
+//<iframe style="display: inline-block" marginheight="0px" hspace="0" vspace="0" marginwidth="0px" frameBorder="0" height="500" width="400" src="https://j-co.ga/BetterGFMotes/Preview/index.html"></iframe>
+
+
+
+
+/*
+var BetterGFMotesBTN = document.createElement("iframe"); //Create button
+
+BetterGFMotesBTN.setAttribute("id", "BetterGFMotesBTN"); // Set button ID
+BetterGFMotesBTN.setAttribute("style", "position:absolute;z-index:102128917297"); // Set CSS
+BetterGFMotesBTN.setAttribute("src", "http://google.com"); // Set code to run on button click
+BetterGFMotesBTN.setAttribute("width", "400px");
+BetterGFMotesBTN.setAttribute("height", "500px");
+*/
+// var BetterGFMotesBTNt = document.createTextNode("jda3GFMotes"); // Set button label
+
+// BetterGFMotesBTN.appendChild(BetterGFMotesBTNt); // Actually adds button label from above ^
+//document.body.appendChild(BetterGFMotesBTN); //Add button to body
+//BetterGFMotesBTN.parentNode.insertBefore(BetterGFMotesBTN, header);
+
+
+//var BetterGFMotesBTN = document.getElementById('intro');
+
+//<link rel="stylesheet" type="text/css" href="https://j-co.ga/BetterGFMotesDB/css/CaesarForMe.css"/>
+    
+// Add this into an element of test to test font additions:
+// <p style="font-family:CaesarForMe;">Kl</p>
