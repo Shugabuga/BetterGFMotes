2d3dfd940d23655ad8fa2958f25e5909c30a5360
Shugabuga <jeffreydivi@icloud.com>
2016-04-11 20:47:44 -0400

    Added debug / beta mode, changed a security policy to allow communication with emote images.

--- Chrome/code.js
+++ Chrome/code.js
@@ -1,12 +1,43 @@
-// My code
+// BetterGFMotes is by /u/HeyItsShuga. You can use this code, and most of the code in BetterGFMotes, under the by-nc-sa 4.0 license.
+
+///////////////////////////////////////////////////////////////////
+//                  Enabling Debug / Beta Mode                   //
+//                                                               //
+// Run the following code in the JavaScript console:             //
+//                                                               //
+// BetterGFMotesDebugEnable();                                   //
+//                                                               //
+// OR use the following bookmarklet:                             //
+//                                                               //
+// javascript:(function()%7BBetterGFMotesDebugEnable()%7D)()     //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+// Debug menu
+
+var BetterGFMotesDebugMenu = document.createElement("div");
+BetterGFMotesDebugMenu.setAttribute("id", "BetterGFMotesDebugMenu")
+BetterGFMotesDebugMenu.setAttribute("class", "BetterGFMotesDebugMenu")
+BetterGFMotesDebugMenu.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid;background-color: #139180; border-width: 5px; border-color: #139180;position:fixed;left: 600px; top: 50px;")
+
+document.body.appendChild(BetterGFMotesDebugMenu);
+
+//var divs = document.getElementsByTagName("BetterGFMotesDebugMenu");
+//BetterGFMotesSelectScreen.parentNode.insertBefore(BetterGFMotesDebugMenu, header);
+
+$("#BetterGFMotesDebugMenu").append('<button onclick="BetterGFMotesEnableSelectScreen();">Show EmoteBox</button> <button onclick="BetterGFMotesDisableSelectScreen();">Hide EmoteBox</button> <button style="background-color:red" onclick="BetterGFMotesDebugDisable();">X</button> <br> <p style="color:white">All settings displayed are either in beta or are incomplete!</p>');
+
+$( "#BetterGFMotesDebugMenu" ).draggable();
+
+// Adds in the "Advanced" Emote Viewer
 
 var BetterGFMotesSelectScreen = document.createElement("IFRAME");
-BetterGFMotesSelectScreen.setAttribute("src", "https://j-co.ga/BetterGFMotesViewer/popup.html");
+BetterGFMotesSelectScreen.setAttribute("src", "https://j-co.ga/BetterGFMotesViewer/index.html");
 BetterGFMotesSelectScreen.setAttribute("width", "500px");
 BetterGFMotesSelectScreen.setAttribute("height", "300px");
 BetterGFMotesSelectScreen.setAttribute("id", "BetterGFMotesSelectScreen")
-BetterGFMotesSelectScreen.setAttribute("class", "BetterGFMotesSelectScreen ui-widget-content")
-BetterGFMotesSelectScreen.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid; border-width: 5px; border-color: #139180;position:fixed;")
+BetterGFMotesSelectScreen.setAttribute("class", "BetterGFMotesSelectScreen")
+BetterGFMotesSelectScreen.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid; border-width: 5px; border-color: #139180;position:fixed;left: 45px; top: 25px;")
 BetterGFMotesSelectScreen.setAttribute("scrolling", "no")
 
 document.body.appendChild(BetterGFMotesSelectScreen);
@@ -24,7 +55,13 @@ var BetterGFMotesEnableSelectScreen = function() { BetterGFMotesSelectScreen.set
 
 var BetterGFMotesDisableSelectScreen = function() { BetterGFMotesSelectScreen.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid; border-width: 5px; border-color: #139180;position:fixed;")  };
 
-// BetterGFMotesEnableSelectScreen(); //We're not going to load it up now, but it can be loaded up if you want!
+var BetterGFMotesDebugEnable = function() { BetterGFMotesDebugMenu.setAttribute("style", "z-index:392639629361637816;border-style: solid;background-color: #139180; border-width: 5px; border-color: #139180;position:fixed;left: 600px; top: 50px;") };
+
+var BetterGFMotesDebugDisable = function() { BetterGFMotesDebugMenu.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid;background-color: #139180; border-width: 5px; border-color: #139180;position:fixed;left: 600px; top: 50px;")  };
+
+//BetterGFMotesDebugEnable(); // Loads up Debug / Beta Mode
+
+// BetterGFMotesEnableSelectScreen(); // We're not going to load it up now, but it can be loaded up if you want!
 
 var script = document.createElement('script');
 script.textContent = 'var BetterGFMotesEnableSelectScreen = function() { BetterGFMotesSelectScreen.setAttribute("style", "z-index:392639629361637816;border-style: solid; border-width: 5px; border-color: #139180;position:fixed;") };';
@@ -36,20 +73,17 @@ script.textContent = 'var BetterGFMotesDisableSelectScreen = function() { Better
 (document.head||document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
 
+var script = document.createElement('script');
+script.textContent = 'var BetterGFMotesDebugEnable = function() { BetterGFMotesDebugMenu.setAttribute("style", "z-index:392639629361637816;border-style: solid;background-color: #139180; border-width: 5px; border-color: #139180;position:fixed;left: 600px; top: 50px;") };';
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
+
+var script = document.createElement('script');
+script.textContent = 'var BetterGFMotesDebugDisable = function() { BetterGFMotesDebugMenu.setAttribute("style", "display:none;z-index:392639629361637816;border-style: solid;background-color: #139180; border-width: 5px; border-color: #139180;position:fixed;left: 600px; top: 50px;") };';
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
+
+
 var BetterGFMotesFrame = document.getElementById('BetterGFMotesSelectScreen');
 
 BetterGFMotesFrame.contentWindow.postMessage(BetterGFMotesDisableSelectScreen(), '*');
-
-// Enabling the secret draggable emote box:
-//
-// Run the following code in the JavaScript console:
-//
-// BetterGFMotesEnableSelectScreen();
-//
-
-// Disabling the secret draggable emote box:
-//
-// Run the following code in the JavaScript console:
-//
-// BetterGFMotesDisableSelectScreen();
-//
